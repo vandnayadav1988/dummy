4a46654207d49ffec1cb126c3e336fe9f19f74b5
Vandna yadav <32866538+vandnayadav1988@users.noreply.github.com>
2023-09-18 09:02:35 +0530

    Update CodeReview.java

--- CodeReview.java
+++ CodeReview.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 class CodeReview {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+int a;
+        
         // Sample code to be reviewed
         String codeToReview = "public class HelloWorld {\n" +
                               "    public static void main(String[] args) {\n" +
